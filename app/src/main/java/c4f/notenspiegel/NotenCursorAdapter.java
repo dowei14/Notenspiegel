ba025ee124cf43751195959d409aa3690785c53b
dowei14 <dominik@weickgenannt.eu>
2017-10-12 07:16:26 +0200

    Überschrift hinzugefügt

--- app/src/main/java/c4f/notenspiegel/NotenCursorAdapter.java
+++ app/src/main/java/c4f/notenspiegel/NotenCursorAdapter.java
@@ -23,22 +23,22 @@ public class NotenCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView nameTextView = (TextView) view.findViewById(R.id.noten_list_fach_name);
+        //TextView nameTextView = (TextView) view.findViewById(R.id.noten_list_fach_name);
         TextView notenNameTextView = (TextView) view.findViewById(R.id.noten_list_noten_name);
         TextView notenTextView = (TextView) view.findViewById(R.id.noten_list_note);
         TextView gewichtungTextView = (TextView) view.findViewById(R.id.noten_list_gewichtung);
 
-        int nameColumnIndex = cursor.getColumnIndex(NotenEntry.COLUMN_FACH_ID);
+        //int nameColumnIndex = cursor.getColumnIndex(NotenEntry.COLUMN_FACH_ID);
         int notenNameColumnIndex = cursor.getColumnIndex(NotenEntry.COLUMN_NOTEN_NAME);
         int noteColumnIndex = cursor.getColumnIndex(NotenEntry.COLUMN_NOTE);
         int gewichtungColumnIndex = cursor.getColumnIndex(NotenEntry.COLUMN_GEWICHTUNG);
 
-        String fachName = cursor.getString(nameColumnIndex);
+        //String fachName = cursor.getString(nameColumnIndex);
         String notenNameName = cursor.getString(notenNameColumnIndex);
         String notenName = Double.toString( cursor.getDouble(noteColumnIndex) / 100);
         String gewichtungName = Double.toString( cursor.getDouble(gewichtungColumnIndex) / 100);
 
-        nameTextView.setText(fachName);
+        //nameTextView.setText(fachName);
         notenNameTextView.setText(notenNameName);
         notenTextView.setText(notenName);
         gewichtungTextView.setText(gewichtungName);
